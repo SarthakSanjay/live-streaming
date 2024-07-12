0548dd940ccd969778dd270d7499b6cd43ff37aa
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-07-12 22:52:15 +0530

    resolve publiv path

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express'
+import http from 'http'
+import path from 'path'
 
 const app = express()
+const server = http.createServer(app)
+
+app.use(express.static(path.resolve('./src/public')))
 
 
-app.listen(3000 , ()=>{
+server.listen(3210 , ()=>{
     console.log('server is running on port 3000');
 })
